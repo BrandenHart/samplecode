db7c52d5ee64055b26afb404faf7b18a02f6eb5b
BrandenHart <102194786+BrandenHart@users.noreply.github.com>
2022-03-22 20:19:23 -0500

    Add files via upload

---
+++ MathOperations Code.ino
@@ -0,0 +1,124 @@
+#include <Wire.h>
+#include <string.h>
+#include "Adafruit_TMP006.h"
+#define USE_USCI_B1 
+
+Adafruit_TMP006 tmp006;
+void printFloat(float value, int places);
+
+#define ledRed RED_LED // red LED
+#define ledGreen GREEN_LED // green LED
+const int buttonOne = 33; // Push button pin J4.33
+const int buttonTwo = 32; // Push button pin J4.32
+const int xpin = 23;      // x-axis of the accelerometer
+const int ypin = 24;     // y-axis
+const int zpin = 25;     // z-axis (only on 3-axis models)
+int a1 = 0;
+int a2 = 0;
+int z = 0;
+int nextTerm = 0;
+
+int buttonOneState = 0; // detects if the button is pushed
+int buttonTwoState = 0; // detects if the button is pushed
+String command; // holds the command 
+boolean stringCorrect = false; // checks if the command entered is acceptable
+int m = 0; // power function variable (base)
+int n = 0; // power function variable (exponent)
+
+void setup() {
+pinMode(ledRed,OUTPUT);
+pinMode(ledGreen,OUTPUT);
+pinMode(buttonOne,INPUT_PULLUP);
+pinMode(buttonTwo,INPUT_PULLUP); 
+
+Serial.begin(9600);
+analogReadResolution(12);
+input.reserve(200); // delegated to the string command
+
+// 
+if (! tmp006.begin(TMP006_CFG_8SAMPLE)) {
+    Serial.println("The sensor is not found");
+    while(1);
+}
+
+}
+
+void loop() {
+  // if button 1 is pressed, display the temperature
+  buttonOneState = digitalRead(buttonOne);
+  if(buttonOneState == LOW) {
+    float objt = tmp006.readObjTempC();
+    Serial.print("Object Temperature: "); Serial.print(objt); Serial.println("*C");
+    Serial.println("\nExecuted");
+    delay(1000);
+    }
+
+    // if button 2 is pressed, display the accelemerometer values
+    buttonTwoState = digitalRead(buttonTwo);
+    if(buttonTwoState == LOW) {
+     Serial.print(((int) analogRead(xpin)) - 2048);
+     // print a tab between values:
+     Serial.print("\t");
+     Serial.print(((int) analogRead(ypin)) - 2048);
+     // print a tab between values:
+     Serial.print("\t");
+     Serial.print(((int) analogRead(zpin)) - 2048);
+     Serial.println(); 
+     Serial.println("\nExecuted")
+     delay(1000);
+     }
+
+     if(Serial.available() > 0) {
+      if(stringCorrect == false)
+      {
+        char commandChar = (char)Serial.read();
+        if(commandChar == '\n')
+        {
+          stringCorrect = true;
+        }
+        else {
+          command +=commandChar;
+        }
+      }
+     }
+        if(stringCorrect == true) {
+          Serial.println(command);
+          if(command == "POW m n R") {
+            int result = 1;
+            for(int i = 0; i < n; ++i) {
+              result*=m;
+              digitalRead(result);
+              digitalWrite(ledRed, HIGH);
+              Serial.println("\nExecuted");
+              }
+          }
+            
+          }
+          else if(command == "SRS n G") {
+           for(int i = 1; i <= z; ++i) {
+             sum = 3*a1[i] - a2[i];
+             nextTerm = sum;
+             digitalRead(nextTerm);
+             digitalWrite(ledGreen, HIGH);
+             Serial.println("\nExecuted");
+           }
+          }
+
+          else {
+          Serial.println("Invalid Command");
+        }
+        stringCorrect = false;
+        command = "";
+        }
+   
+      
+     
+      
+      
+      
+      
+
+     
+  
+  
+  
